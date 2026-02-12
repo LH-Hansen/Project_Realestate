95394ef4f805a6b772b8a41b4634b5f068641527
LH-Hansen <lasse.hh@protonmail.com>
2026-02-12 20:41:59 +0100

    Added secure storage w/ login/out

---
+++ RealEstateApp/Models/LoginResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RealEstateApp.Models;
+
+public class LoginResult
+{
+    public bool Succeeded { get; set; }
+    public string AccessToken { get; set; }
+    public string RefreshToken { get; set; }
+}
---
+++ RealEstateApp/ViewModels/LoginViewModel.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Maui.Storage;
+using RealEstateApp.Models;
+using System.Windows.Input;
+
+namespace RealEstateApp.ViewModels;
+
+public class LoginViewModel : BaseViewModel
+{
+    const string AccessKey = "access_token";
+    const string RefreshKey = "refresh_token";
+
+    public string Username { get; set; }
+    public string Password { get; set; }
+
+    public ICommand LoginCommand =>
+        new Command(async () => await LoginAsync());
+
+    public async Task LoginAsync()
+    {
+        var result = Authenticate();
+
+        if (!result.Succeeded)
+        {
+            await Shell.Current.DisplayAlert(
+                "Login failed",
+                "Invalid credentials",
+                "OK");
+            return;
+        }
+
+        await SecureStorage.Default.SetAsync(
+            AccessKey,
+            result.AccessToken);
+
+        await SecureStorage.Default.SetAsync(
+            RefreshKey,
+            result.RefreshToken);
+
+        await Shell.Current.GoToAsync("//propertylist");
+    }
+
+    LoginResult Authenticate()
+    {
+        if (Username == "admin" && Password == "admin")
+        {
+            var rnd = new Random();
+
+            return new LoginResult
+            {
+                Succeeded = true,
+                AccessToken = rnd.Next().ToString(),
+                RefreshToken = rnd.Next().ToString()
+            };
+        }
+
+        return new LoginResult { Succeeded = false };
+    }
+
+    public static async Task<bool> HasSavedLogin()
+    {
+        var token =
+            await SecureStorage.Default.GetAsync(AccessKey);
+
+        return token != null;
+    }
+
+    public static void Logout()
+    {
+        SecureStorage.Default.Remove(AccessKey);
+        SecureStorage.Default.Remove(RefreshKey);
+    }
+}
