c46ee5c33baf8bab9c5369c8110cb6fb0bf30975
LH-Hansen <lasse.hh@protonmail.com>
2026-02-12 20:32:18 +0100

    Added settings for Text to speech.

---
+++ RealEstateApp/Views/SettingsPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using RealEstateApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RealEstateApp.Views
+{
+    public partial class SettingsPage : ContentPage
+    {
+        SettingsViewModel vm;
+
+        public SettingsPage(SettingsViewModel vm)
+        {
+            InitializeComponent();
+            BindingContext = this.vm = vm;
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            vm.LoadSettings();
+        }
+
+        protected override void OnDisappearing()
+        {
+            vm.SaveSettings();
+            base.OnDisappearing();
+        }
+    }
+
+}
